f4934e3df9c7797f9c75811bdd9575be2b3575ab
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-10-02 13:10:08 -0300

    refactor: update CharacterCard component with badges and style adjustments

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link"
 import React from "react"
+import { Badge } from "@components/Badge"
 
 interface CharacterCardProps {
     character: {
@@ -11,99 +13,22 @@ interface CharacterCardProps {
     }
 }
 
-export const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
-    let badgeClass = "badge border-0 text-white"
-    let speciesClass = "badge border-0 text-white"
-    let genderClass = "badge border-0 text-white"
-
-    switch (character.status) {
-        case "Alive":
-            badgeClass += " bg-green-500"
-            break
-        case "Dead":
-            badgeClass += " bg-red-500"
-            break
-        case "unknown":
-            badgeClass += " bg-gray-500"
-            break
-        default:
-            badgeClass += " bg-gray-300"
-            break
-    }
-
-    switch (character.species) {
-        case "Human":
-            speciesClass += " bg-blue-500"
-            break
-        case "Alien":
-            speciesClass += " bg-purple-500"
-            break
-        case "Humanoid":
-            speciesClass += " bg-green-700"
-            break
-        case "unknown":
-            speciesClass += " bg-gray-500"
-            break
-        case "Poopybutthole":
-            speciesClass += " bg-yellow-500"
-            break
-        case "Mythological Creature":
-            speciesClass += " bg-yellow-700"
-            break
-        case "Animal":
-            speciesClass += " bg-brown-500"
-            break
-        case "Robot":
-            speciesClass += " bg-gray-700"
-            break
-        case "Cronenberg":
-            speciesClass += " bg-red-700"
-            break
-        case "Disease":
-            speciesClass += " bg-green-500"
-            break
-        default:
-            speciesClass += " bg-gray-300"
-            break
-    }
-
-    switch (character.gender) {
-        case "Male":
-            genderClass += " bg-blue-600"
-            break
-        case "Female":
-            genderClass += " bg-pink-600"
-            break
-        case "unknown":
-            genderClass += " bg-gray-600"
-            break
-        case "Genderless":
-            genderClass += " bg-green-600"
-            break
-        default:
-            genderClass += " bg-yellow-600"
-            break
-    }
-
+export const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {   
     return (
-        <div className="card bg-base-100 shadow-xl hover:scale-105 cursor-pointer w-full sm:w-auto mx-auto" style={{ maxWidth: '320px' }}>
-            <figure className="w-full overflow-hidden">
-                <img src={character.image} alt={`${character.name} image`} className="w-full h-full object-cover" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{character.name}</h2>
-                <div className="flex space-x-2">
-                    <p className={badgeClass} aria-label={`Status: ${character.status}`}>
-                        {character.status}
-                    </p>
-                    <p className={speciesClass} aria-label={`Species: ${character.species}`}>
-                        {character.species}
-                    </p>
-                    <p className={genderClass} aria-label={`Gender: ${character.gender}`}>
-                        {character.gender}
-                    </p>
+        <Link href={`/character/${character.id}`} passHref>
+            <div className="card bg-base-100 shadow-xl hover:scale-105 cursor-pointer w-full sm:w-auto mx-auto" style={{ maxWidth: '320px', height: '470px' }}>
+                <figure className="w-full overflow-hidden">
+                    <img src={character.image} alt={`${character.name} image`} className="w-full h-full object-cover" />
+                </figure>
+                <div className="card-body flex flex-col justify-between">
+                    <h2 className="card-title">{character.name}</h2>
+                    <div className="flex gap-2 flex-wrap">
+                        <Badge type="status" value={character.status} />
+                        <Badge type="species" value={character.species} />
+                        <Badge type="gender" value={character.gender} />
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
