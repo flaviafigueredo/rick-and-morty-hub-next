54eda83d0d9d46ec7b860260aceddce7838ac982
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-10-04 10:35:09 -0300

    feat: add specific error message

--- app/hooks/useCharacters.ts
+++ app/hooks/useCharacters.ts
@@ -9,17 +9,21 @@ interface UseCharactersResult {
 
 export async function useCharacters(pageID: number, nameFilter?: string): Promise<UseCharactersResult> {
     try {
-        const query = nameFilter 
-            ? `/character?name=${nameFilter}&page=${pageID}` 
+        const query = nameFilter
+            ? `/character?name=${nameFilter}&page=${pageID}`
             : `/character?page=${pageID}`
-        
+
         const response = await api.get(query)
-        
+
         const characters: Character[] = response.data.results
         const totalPages: number = response.data.info.pages
         return { data: characters, totalPages }
     } catch (error: any) {
-        const errorMessage: string = error.message
+        if (error.response?.status === 404) {
+            return { error: "Character not found. Please try again." }
+        }
+
+        const errorMessage: string = error.message || "An unexpected error occurred."
         return { error: errorMessage }
     }
 }
