602c89360d5f9394e1f6b957d35f6bbd5fc8c7cc
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-10-04 12:01:53 -0300

    refactor(footer): update Footer component layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,13 +2,19 @@ import Image from "next/image"
 import footerImg from "@assets/footer-img.png"
 
 export const Footer = () => {
-    const currentYear = new Date().getFullYear();
-
     return (
-        <footer className="flex flex-col sm:flex-row justify-center items-center gap-2 py-4 space-x-2 border-t border-gray-300">
-            <div className="text-center">
-                <p className="text-sm">© {currentYear} All rights reserved.</p>
+        <footer className="footer bg-neutral text-neutral-content flex flex-col sm:flex-row justify-between items-center p-4">
+            <aside className="flex items-center gap-2 mb-2 sm:mb-0">
+                <Image
+                    src={footerImg}
+                    alt="Rick and Morty's dog"
+                    width={100}
+                    className="filter drop-shadow-lg"
+                />
+                <p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
+            </aside>
 
+            <div className="text-center">
                 <p className="text-sm">
                     Developed with 🩶 by <a href="https://flaviafigueredo.github.io/portfolio/"
                         target="_blank"
@@ -18,13 +24,6 @@ export const Footer = () => {
                     </a>
                 </p>
             </div>
-
-            <Image
-                src={footerImg}
-                alt="Rick and Morty's dog"
-                width={100}
-                className="filter drop-shadow-lg"
-            />
         </footer>
     )
 }
