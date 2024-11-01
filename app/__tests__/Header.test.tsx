b4e0c114d0dcd933a1297e8e1e032ae858029f5a
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-11-01 11:23:29 -0300

    test: update Header tests to use SearchContext

--- app/__tests__/Header.test.tsx
+++ app/__tests__/Header.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen, fireEvent, act } from '@testing-library/react';
 import { ReactNode, AnchorHTMLAttributes } from 'react';
 import { Header } from '@components/Header';
+import { SearchProvider } from 'context/SearchContext';
+import { useRouter } from 'next/navigation';
 
 jest.mock('next/link', () => {
     return ({ children, ...props }: { children: ReactNode } & AnchorHTMLAttributes<HTMLAnchorElement>) => (
@@ -10,11 +12,22 @@ jest.mock('next/link', () => {
 jest.mock('next/image', () => (props: any) => <img {...props} />);
 jest.mock('@assets/icon.png', () => 'logo.png');
 
+jest.mock('next/navigation', () => ({
+    useRouter: jest.fn(),
+}));
+
 describe('Header component', () => {
     it('should render the header with logo, theme toggle button, and search form', () => {
-        const mockOnSearch = jest.fn();
+        const pushMock = jest.fn();
+        (useRouter as jest.Mock).mockReturnValue({
+            push: pushMock,
+        });
 
-        render(<Header onSearch={mockOnSearch} />);
+        render(
+            <SearchProvider>
+                <Header />
+            </SearchProvider>
+        );
 
         const logoImage = screen.getByAltText(/rick and morty logo/i);
         expect(logoImage).toBeInTheDocument();
@@ -26,20 +39,36 @@ describe('Header component', () => {
         expect(searchInput).toBeInTheDocument();
     });
 
-    it('should call onSearch with an empty string when logo is clicked', () => {
-        const mockOnSearch = jest.fn();
-        render(<Header onSearch={mockOnSearch} />);
+    it('should call handleSearch with an empty string when logo is clicked', () => {
+        const pushMock = jest.fn();
+        (useRouter as jest.Mock).mockReturnValue({
+            push: pushMock,
+        });
+
+        render(
+            <SearchProvider>
+                <Header />
+            </SearchProvider>
+        );
 
         const logoLink = screen.getByLabelText(/go to homepage/i);
 
         fireEvent.click(logoLink);
 
-        expect(mockOnSearch).toHaveBeenCalledWith('');
+        expect(pushMock).toHaveBeenCalledWith('/?search=');
     });
 
     it('should reflect the correct theme in the toggle button', () => {
-        const mockOnSearch = jest.fn();
-        render(<Header onSearch={mockOnSearch} />);
+        const pushMock = jest.fn();
+        (useRouter as jest.Mock).mockReturnValue({
+            push: pushMock,
+        });
+
+        render(
+            <SearchProvider>
+                <Header />
+            </SearchProvider>
+        );
 
         const toggleInput = screen.getByRole('checkbox');
 
